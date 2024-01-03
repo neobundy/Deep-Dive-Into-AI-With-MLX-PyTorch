409664536c963d32ada6fc641bdb38c5f8452bd6
Nicolò Carandini <ncarandini@users.noreply.github.com>
2024-01-03 20:05:51 +0100

    Update story-of-a-tensor-torch.py
    Warning: I'm a rookie on python and torch, so I've modified the source on VS Code using the "hello-ai-world-torch.py" file as a guide and many requests to GitHub Copilot selecting portion of the original code and asking "Do the same with Torch" 😄
    After some iteration I made it working. I wish I coud use a Mac to check if my version returns equivalent output, especially regarding output of weight and bias parameters.

--- book/001-story-of-a-tensor/story-of-a-tensor-torch.py
+++ book/001-story-of-a-tensor/story-of-a-tensor-torch.py
@@ -1,21 +1,19 @@
-import mlx.core as mx
-import mlx.nn as nn
-import mlx.optimizers as optim
+import torch
+import torch.nn as nn
 
 # Tenny is a tensor with random numbers
-Tenny = mx.random.normal(shape=[10])
+Tenny = torch.randn(10)
 
 # Tenny reshapes himself into a vector, a matrix, and a 3D array
 vector = Tenny.reshape(-1)
 matrix = Tenny.reshape(5, 2)
 three_d_array = Tenny.reshape(2, 5, 1)
 
-
 # Define a simple linear regression model
 class GreatNeuralNetwork(nn.Module):
     def __init__(self):
         super(GreatNeuralNetwork, self).__init__()
-        self.linear = nn.Linear(input_dims=1, output_dims=1)  # Weighy and Bitsy Bias are part of this
+        self.linear = nn.Linear(in_features=1, out_features=1)  # Weighy and Bitsy Bias are part of this
 
     def __call__(self, x):
         x = self.linear(x)
@@ -24,27 +22,27 @@ class GreatNeuralNetwork(nn.Module):
 model = GreatNeuralNetwork()
 
 # Print Weighy and Bitsy Bias
-for name, param in model.parameters().items():
+
+for name, param in model.named_parameters():
     print(name, param)
 
 # Define a loss function and an optimizer
-def loss_fn(model, x, y):
-    return nn.losses.mse_loss(model(x), y)
-
-loss_and_grad_fn = nn.value_and_grad(model, loss_fn)
-optimizer = optim.SGD(learning_rate=0.01)
+loss_function = nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
 # Training data
-x_train = mx.random.normal([10, 1])
+x_train = torch.randn(10, 1)
 y_train = 3*x_train + 2
 
 # Training process
 for epoch in range(100):
-    loss, grads = loss_and_grad_fn(model, x_train, y_train)
-    optimizer.update(model, grads)
-    mx.eval(model.parameters(), optimizer.state)
+    predictions = model(x_train)
+    loss = loss_function(predictions, y_train)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
 
 # Make a prediction
-x_test = mx.array([[5.0]])
+x_test = torch.tensor([[5.0]])
 y_test = model(x_test)
 print(f"Prediction: {y_test.item()}")
